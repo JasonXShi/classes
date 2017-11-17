e52e27d54dc587198e803d4c83e0ae080476ffdf
Jason Shi <jasonxshi2002@gmail.com>
2017-11-16 20:10:37 -0800

    Update movie.h

--- movie.h
+++ movie.h
@@ -1,3 +1,4 @@
+//header guards and includes
 #ifndef MOVIE_H
 #define MOVIE_H
 #include "digitalMedia.h"
@@ -5,14 +6,17 @@
 
 class Movie:public DigitalMedia{
  public:
+ //defines constructor and getters
   Movie(char* newTitle,int newYear,  char* director, int duration, float newRating);
   int getDuration();
   char* getDirector();
   float getRating();
+ //defines other functions like the destructor, displaying info, and getting the type
   void displayInfo();
   int getType();
   ~Movie();
  private:
+ //makes the variables unique to movies
   char* director;
   int duration;
   float rating;

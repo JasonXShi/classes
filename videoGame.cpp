bbe1cc973d7096a1f4ca95ad21996657093e2fb4
Jason Shi <jasonxshi2002@gmail.com>
2017-11-16 20:14:01 -0800

    Update videoGame.cpp

--- videoGame.cpp
+++ videoGame.cpp
@@ -1,13 +1,14 @@
+//includes useful code
 #include <cstring>
 #include "videogame.h"
 #include <iostream>
 using namespace std;
-
+//constructor for videogame, also calls the constructor of digitalMedia for the title and year
 VideoGame::VideoGame(char* newTitle, int newYear, char* newPublisher, float newRating) : DigitalMedia(newTitle, newYear){
   publisher = newPublisher;
   rating = newRating;
 }
-
+//making getters
 char* VideoGame::getPublisher(){
   return publisher;
 }
@@ -15,17 +16,18 @@ char* VideoGame::getPublisher(){
 float VideoGame::getRating(){
   return rating;
 }
-
+//prints out info
 void VideoGame::displayInfo(){
   cout << "Title: " << getTitle() << endl;
   cout << "Year: " << getYear() << endl;
   cout << "Publisher: " << getPublisher() << endl;
   cout << "Rating: " << getRating() << endl;
 }
-
+//returning 3 for a videoGame
 int VideoGame::getType(){
   return 3;
 }
+//destructor for videogame, deletes the pointer of publisher
 VideoGame::~VideoGame(){
   delete []publisher;
 }

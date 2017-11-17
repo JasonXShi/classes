728732b0650875edc92264e40705e426aa60678b
Jason Shi <jasonxshi2002@gmail.com>
2017-11-16 20:12:53 -0800

    Update music.h

--- music.h
+++ music.h
@@ -1,3 +1,4 @@
+//makes the header guards and includes code
 #ifndef MUSIC_H
 #define MUSIC_H
 #include "digitalMedia.h"
@@ -5,16 +6,19 @@
 
 class Music:public DigitalMedia{
  public:
+ //defines the constructor and getters
   Music(char* newTitle, int newYear, char* newArtist, char* newPublisher, int newDuration);
   char* getArtist();
   char* getPublisher();
   int getDuration();
+ //defines the other functions -destructor, getting the type, and printing out the info
   void displayInfo();
   int getType();
   ~Music();
  private:
+ //makes the variables that are unique to music
   char* artist;
-  char* publisher ;
+  char* publisher;
   int duration;
 };
 

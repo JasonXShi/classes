b825f938dfc1f7f81d2f5db7e3c6ac458561f25a
Jason Shi <jasonxshi2002@gmail.com>
2017-11-16 19:54:19 -0800

    Update digitalMedia.h

--- digitalMedia.h
+++ digitalMedia.h
@@ -1,17 +1,19 @@
-//header guards
+//header guards and includes
 #ifndef DIGITALMEDIA_H
 #define DIGITALMEDIA_H
 #include <cstring>
 
 class DigitalMedia{
 	public:
+	//defining deconstructors, getters, and constructor.
+	//also other functions like displayInfo and getType
 	  DigitalMedia(char* newTitle, int newYear);
 	  virtual ~DigitalMedia();
 	  char* getTitle();
 	  int getYear();
 	  void displayInfo();
 	  virtual int getType();
-	private:
+	private: //variables that are used in all the child classes
 	  char* title;
 	  int year;
 };

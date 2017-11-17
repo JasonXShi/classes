7e0fc3914c308869b5cafe7560b0547d6110f3ec
Jason Shi <jasonxshi2002@gmail.com>
2017-11-16 20:11:38 -0800

    Update music.cpp

--- music.cpp
+++ music.cpp
@@ -1,14 +1,15 @@
+//includes needed code
 #include <cstring>
 #include "music.h"
 #include <iostream>
 using namespace std;
-
+//constructor for music, also calls the constructor of the parent class (digitalMedia)
 Music::Music(char* newTitle, int newYear, char* newArtist, char* newPublisher, int newDuration ) : DigitalMedia(newTitle, newYear){
   artist = newArtist;
   publisher = newPublisher;
   duration = newDuration;
 }
-
+//makes getters
 char* Music::getArtist(){
   return artist;
 }
@@ -20,10 +21,12 @@ char* Music::getPublisher(){
 int Music::getDuration(){
   return duration;
 }
+//destructor: deletes the pointers
 Music::~Music(){
   delete[] publisher;
   delete[] artist;
 }
+//displays the info of music
 void Music::displayInfo(){
   cout << "Title: " << getTitle() << endl;
   cout << "Year: " << getYear() << endl;
@@ -31,7 +34,7 @@ void Music::displayInfo(){
   cout << "Publisher: " << getPublisher() << endl;
   cout << "Duration: " << getDuration() << endl;
 }
-
+//returns '2' if it is music
 int Music::getType(){
   return 2;
 }

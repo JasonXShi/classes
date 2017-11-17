8022e2dac7790b649b0d13a020e3a58639043930
Jason Shi <jasonxshi2002@gmail.com>
2017-11-16 20:09:34 -0800

    Update movie.cpp

--- movie.cpp
+++ movie.cpp
@@ -1,14 +1,15 @@
+//includes needed stuff
 #include <iostream>
 #include <cstring>
 #include "movie.h"
 using namespace std;
-
+//make constructor, it also calls the constructor of the parent class
 Movie::Movie(char* newTitle, int newYear, char* newDirector, int newDuration, float newRating) : DigitalMedia(newTitle, newYear){
   director = newDirector;
   duration = newDuration;
   rating = newRating;
 }
-
+//makes getters
 char* Movie::getDirector(){
   return director;
 }
@@ -20,7 +21,7 @@ int Movie::getDuration(){
 float Movie::getRating(){
   return rating;
 }
-
+//display the info of a movie
 void Movie::displayInfo(){
   cout << "Title: " << getTitle() << endl;
   cout << "Year: " << getYear() << endl;
@@ -28,10 +29,11 @@ void Movie::displayInfo(){
   cout << "Duration: " << getDuration() << endl;
   cout << "Rating: " << getRating() << endl;
 }
-
+//return '1' if it is a movie
 int Movie::getType(){
   return 1;
 }
+//destructor for movie
 Movie::~Movie(){
   delete[] director;
 }

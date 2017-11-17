9c33159021347ce54381918c5c62fc45880d60b5
Jason Shi <jasonxshi2002@gmail.com>
2017-11-16 20:08:26 -0800

    Update videoGame.h

--- videoGame.h
+++ videoGame.h
@@ -1,3 +1,4 @@
+//make header guards and include 
 #ifndef VIDEOGAME_H
 #define VIDEOGAME_H
 #include "digitalMedia.h"
@@ -5,13 +6,16 @@
 
 class VideoGame:public DigitalMedia{
  public:
+ //defines the constructor and getters
   VideoGame(char* newTitle, int newYear, char* newPublisher, float newRating);
   char* getPublisher();
   float getRating();
+ //defines the methods such as displaying info, getting type, and deconstructor
   void displayInfo();
   int getType();
   ~VideoGame();
  private:
+ //the variables that are unique in a videogame
   char* publisher;
   float rating;
 };

757fdff4ec03fc07f6c29bf0ff65d4b8d1644547
Jason Shi <jasonxshi2002@gmail.com>
2017-11-16 19:48:22 -0800

    Update digitalMedia.cpp

--- digitalMedia.cpp
+++ digitalMedia.cpp
@@ -2,12 +2,12 @@
 #include <cstring>
 #include <iostream>
 using namespace std;
-
+//making constructor
 DigitalMedia::DigitalMedia(char* newTitle, int newYear) {
   title = newTitle;
   year = newYear;
 }
-
+//making getters
 char* DigitalMedia::getTitle(){
   return title;
 }
@@ -15,16 +15,16 @@ char* DigitalMedia::getTitle(){
 int DigitalMedia::getYear(){
   return year;
 }
-
+//making deconstructor
 DigitalMedia::~DigitalMedia() {
   delete []title;
 }
-
+//prints out info
 void DigitalMedia::displayInfo(){
   cout << "Title: " << getTitle() << endl;
   cout << "Year: " << getYear() << endl;
 }
-
+//returns type
 int DigitalMedia::getType(){
   return 0;
 }
